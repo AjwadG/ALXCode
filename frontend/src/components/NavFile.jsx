8bc7f1cff6804b9e3ab820d9b393bebbf0100c8a
hamzahdili2001 <hdilihamza955@gmail.com>
2024-07-07 23:48:47 +0100

    fix nav files style

--- frontend/src/components/NavFile.jsx
+++ frontend/src/components/NavFile.jsx
@@ -3,7 +3,7 @@ import { FaFile } from "react-icons/fa";
 
 function NavFile({ file, setNavFiles, activeFile }) {
   const classNames =
-    "flex items-center justify-between text-gray-400 h-full p-3 min-w-28 ".concat(
+    "flex items-center justify-between text-gray-400 h-full p-3 min-w-28 gap-3 ".concat(
       activeFile ? "bg-zinc-800" : "bg-zinc-900"
     );
   return (
